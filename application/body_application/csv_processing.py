f0df0a4e67fecaef43bc3180a932ca1c0d41198e
ptizaS <ptizay@gmail.com>
2023-01-24 01:29:46 +0100

    hw5 -> correct the mistake, modified the code relative to the new CSV-file.

--- application/body_application/csv_processing.py
+++ application/body_application/csv_processing.py
@@ -10,16 +10,16 @@ def to_calculation_average(name_file: str = None) -> dict:
         sum_of_people_height = 0
         sum_of_people_weight = 0
         for line in reader_csv:
-            sum_of_people_height += float(line[' "Height(Inches)"'].strip())
-            sum_of_people_weight += float(line[' "Weight(Pounds)"'].strip())
+            sum_of_people_height += float(line["Height(Inches)"])
+            sum_of_people_weight += float(line["Weight(Pounds)"])
         average_height_in_cm = round((sum_of_people_height / int(line["Index"])) * 2.54, 2)
         average_weight_in_kg = round((sum_of_people_weight / int(line["Index"])) * 0.45, 2)
         return {"average_height_in_cm": average_height_in_cm, "average_weight_in_kg": average_weight_in_kg}
 
 
 #
-def output_after_processing_csv(output_info: dict = None) -> None:
-    return print(
+def output_after_processing_csv(output_info: dict) -> None:
+    print(
         f"""After processing the csv-file, we got the following values:
 Average height of people: {output_info["average_height_in_cm"]} cm.
 Average weight of people: {output_info["average_weight_in_kg"]} kg."""
@@ -27,4 +27,4 @@ Average weight of people: {output_info["average_weight_in_kg"]} kg."""
 
 
 if __name__ == "__main__":
-    output_after_processing_csv()
+    output_after_processing_csv(to_calculation_average(name_file="output"))
